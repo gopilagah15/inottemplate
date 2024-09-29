8db1a877b43830a0c5e12846cf0f56eec4014c54
gopilagah15 <157889090+gopilagah15@users.noreply.github.com>
2024-09-29 12:37:17 +0530

    dondndn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Home from './component/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './component/Navbar';
+import About from './component/About';
+
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <>
+      <BrowserRouter> 
+      <Navbar/>
+      <h1>iNotebook</h1>
+      <Routes>
+          <Route exact path='/home' element={<Home />} /> 
+          <Route exact path='/about' element={<About />} /> 
+          </Routes> 
+    </BrowserRouter> 
+    </>
   )
 }
 
--- src/component/Addnote.js
+++ src/component/Addnote.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React  from 'react' 
 
 const Addnote = () => {
+
   return (
     <div>
          <form onSubmit={handleClick}> 
